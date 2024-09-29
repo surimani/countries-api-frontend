d9a8acc0213defcf383439d47f2e8da586acda5d
Suresh J <suri.techie@gmail.com>
2024-09-29 22:00:28 +0530

    Removing unwanted files

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -64,11 +64,7 @@ const CountryList: React.FC = () => {
         </div>    
         <div className="flex flex-col md:flex-row w-full md:w-1/2 md:space-x-4 space-y-4 md:space-y-0 h-full">
             <RegionFilter selectedRegion={selectedRegion} setSelectedRegion={setSelectedRegion} />
-            <TimeZoneFilter
-                selectedTimeZone={selectedTimeZone}
-                setSelectedTimeZone={setSelectedTimeZone}
-                availableTimeZones={availableTimeZones}
-            />
+            <TimeZoneFilter selectedTimeZone={selectedTimeZone} setSelectedTimeZone={setSelectedTimeZone} availableTimeZones={availableTimeZones} />
         </div>
         
       </div>
